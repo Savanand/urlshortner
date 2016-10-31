c8516f311057b4eeab99e4fdfdbbe9d3fbba517a
Aniket Savanand <aniketpsavanand@gmail.com>
2016-10-31 16:36:59 -0700

    Handling failures to models

--- shortener/models.py
+++ shortener/models.py
@@ -4,6 +4,11 @@ from django.db import models
 class KirrURL(models.Model):
     url = models.CharField(max_length=220,)
     shortcode = models.CharField(max_length=15, unique=True)
+    updated = models.DateTimeField(auto_now=True) #everytime when model is last saved
+    timestamp = models.DateTimeField(auto_now_add=True) #when model was created
+
+    #empty_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False)  # we can set our own date
+
     def __str__(self):
         return str(self.url)
 
