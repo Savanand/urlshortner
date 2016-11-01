b966b3f9892ceb19221ea78feb8a2c8afb08bf93
Aniket Savanand <aniketpsavanand@gmail.com>
2016-10-31 17:26:48 -0700

    override save method- added base code

--- shortener/models.py
+++ shortener/models.py
@@ -1,6 +1,17 @@
 from django.db import models
-
+import random
+import string
 # Create your models here.
+
+# method to generate string of random 6 characters
+def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    # equivalent for loop would be
+    # for _ in range(size):
+    #     new_code += random.choice(chars)
+    # return new_code
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
 class KirrURL(models.Model):
     url = models.CharField(max_length=220,)
     shortcode = models.CharField(max_length=15, unique=True)
@@ -8,6 +19,12 @@ class KirrURL(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True) #when model was created
 
     #empty_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False)  # we can set our own date
+
+    #over-riding inbuilt save method
+    def save(self, *args, **kwargs):
+        print ("something")
+        self.shortcode = code_generator()
+        super(KirrURL, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.url)
